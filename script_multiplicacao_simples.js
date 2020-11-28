42284b4776d5e944421a1e3a49f08d1c63a4397b
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2020-11-28 13:25:47 -0300

    MultiplicacaoSimples3

--- script_multiplicacao_simples.js
+++ script_multiplicacao_simples.js
@@ -1,12 +1,12 @@
-//Solução 1
+//SOLUCAO 1
 var valor1 = gets();
 var valor2 = gets();
 var total = valor1 * valor2;
 console.log("PROD = " + total);
 
-//Solução 2 - Resumido
+//SOLUCAO 2 - Resumido
 var total = gets() * gets();
 console.log("PROD = " + total);
 
-//Solução 3 - Resumido 
+//SOLUCAO 3 - Resumido 
 console.log("PROD = " + (gets() * gets()));
